669232d6ab94d05d3c0e8929dfb6eea53f6c7725
Donghoon Nam <dhoon.nam@gmail.com>
2019-03-28 17:44:32 +0000

    Refactoring
    code has been refactored.
    
    debugging build has been added:
    command: make debug

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
 
 debug:
-	EXTRA_CFLAGS=-DDEBUG make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
+	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) EXTRA_CFLAGS="-DDEBUG" modules
 
 clean:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
--- stealthhello.c
+++ stealthhello.c
@@ -12,17 +12,17 @@
 #define TLS_DEFAULT_PORTNUM 443
 
 struct tlshdr {
-	u8 protocol;
+	__u8 protocol;
 #define TLSPROTO_HANDSHAKE 22
-	u16 version;
-	u16 len;
+	__u16 version;
+	__u16 len;
 	/* The protocol messages start here. */
 } __attribute__((packed));
 
 struct tls_hs_msghdr {
-	u32 type : 8,
+	__u32 type : 8,
 #define TLS_HS_MSGTYPE_CLIENTHELLO 1
-		len : 24;
+		  len : 24;
 } __attribute__((packed));
 
 static void ip_copy_metadata(struct sk_buff *to, struct sk_buff *from)
@@ -50,53 +50,53 @@ static void ip_copy_metadata(struct sk_buff *to, struct sk_buff *from)
 	skb_copy_secmark(to, from);
 }
 
-static struct sk_buff *ip_tcp_frag(struct net *net, struct sock *sk, struct sk_buff *from,
-		unsigned int begin, unsigned int end)
+static struct sk_buff *tcp_frag(struct net *net, struct sock *sk, struct sk_buff *from,
+		int begin, int end)
 {
 	struct sk_buff *skb;
 	struct iphdr *iph = ip_hdr(from);
-	struct tcphdr *tcph = (struct tcphdr *)skb_transport_header(from);
-	unsigned int iph_l, tcph_l, ll_rs, payload_l, len;
-	int ptr;
+	struct tcphdr *tcph = tcp_hdr(from);
+	int iph_l, tcph_l, hdr_l, ll_rs, payload_l, len;
 	struct rtable *rt = skb_rtable(from);
-	__u32 seq, ack_seq;
-	
-	iph_l = iph->ihl << 2;
-	tcph_l = tcph->doff << 2;
-	
-	payload_l = ntohs(iph->tot_len) - iph_l - tcph_l;
+	__u32 seq;
+	__be32 ack_seq;
+
+	iph_l = ip_hdrlen(from);
+	tcph_l = tcp_hdrlen(from);
+	hdr_l = iph_l + tcph_l;
+
+	payload_l = ntohs(iph->tot_len) - hdr_l;
 
 	seq = ntohl(tcph->seq);
-	ack_seq = ntohl(tcph->ack_seq);
+	ack_seq = tcph->ack_seq;
 
-	ptr = iph_l + tcph_l;
 	len = end - begin;
 	ll_rs = LL_RESERVED_SPACE(rt->dst.dev);
 
-	skb = alloc_skb(iph_l + tcph_l + len + ll_rs, GFP_ATOMIC);
+	skb = alloc_skb(hdr_l + len + ll_rs, GFP_ATOMIC);
 	if (!skb)
 		return 0;
 
 	ip_copy_metadata(skb, from);
 	skb_reserve(skb, ll_rs);
-	skb_put(skb, iph_l + tcph_l + len);
+	skb_put(skb, hdr_l + len);
 	skb_reset_network_header(skb);
 	skb->transport_header = skb->network_header + iph_l;
 
 	if (from->sk)
 		skb_set_owner_w(skb, from->sk);
 
-	skb_copy_from_linear_data(from, skb_network_header(skb), iph_l + tcph_l);
-	if (skb_copy_bits(from, ptr + begin, skb_transport_header(skb) + tcph_l, len))
+	skb_copy_from_linear_data(from, skb_network_header(skb), hdr_l);
+	if (skb_copy_bits(from, hdr_l + begin, skb_transport_header(skb) + tcph_l, len))
 		BUG();
 
-	tcph = (struct tcphdr *)skb_transport_header(skb);
+	tcph = tcp_hdr(skb);
 	tcph->seq = htonl(seq + begin);
-	tcph->ack_seq = htonl(ack_seq);
+	tcph->ack_seq = ack_seq;
 
 	iph = ip_hdr(skb);
-	iph->tot_len = htons(iph_l + tcph_l + len);
-	
+	iph->tot_len = htons(hdr_l + len);
+
 	tcph->check = 0;
 	skb->csum = csum_partial((unsigned char *)tcph, tcph_l + len, 0);
 	tcph->check = tcp_v4_check(
@@ -105,17 +105,17 @@ static struct sk_buff *ip_tcp_frag(struct net *net, struct sock *sk, struct sk_b
 			iph->daddr,
 			skb->csum);
 	ip_send_check(iph);
-/*
+#ifdef DEBUG
 	printk("stealthhello: ori.seq: %u, ori.ack_seq: %u, seq: %u, ack_seq: %u\n",
 			ntohl(seq), ntohl(ack_seq), ntohl(tcph->seq), ntohl(tcph->ack_seq));
-*/
+#endif
 	return skb;
 }
 
 static unsigned int stealth_hello(struct sk_buff *skb, const struct nf_hook_state *state)
 {
 	struct iphdr *iph;
-	unsigned int iph_l, tot_l, tcph_l, mtu, ptr;
+	int tot_l, hdr_l, mtu, ptr;
 	struct sk_buff *frag_skb;
 	int err;
 
@@ -123,24 +123,22 @@ static unsigned int stealth_hello(struct sk_buff *skb, const struct nf_hook_stat
 			(err = skb_checksum_help(skb)))
 		goto fail;
 
-	iph = ip_hdr(skb);
-	iph_l = iph->ihl << 2;
-	tot_l = ntohs(iph->tot_len);
-	tcph_l = tcp_hdrlen(skb);
-
-
 	mtu = ip_skb_dst_mtu(state->sk, skb);
 	if (IPCB(skb)->frag_max_size && IPCB(skb)->frag_max_size < mtu)
 		mtu = IPCB(skb)->frag_max_size;
 
+	iph = ip_hdr(skb);
+	tot_l = ntohs(iph->tot_len);
+
 	if (mtu <= FRAG_SIZE || tot_l <= FRAG_SIZE)
 		goto fail;
 
-	ptr = FRAG_SIZE - iph_l - tcph_l;
-	frag_skb = ip_tcp_frag(state->net, state->sk, skb, 0, ptr);
+	hdr_l = ip_hdrlen(skb) + tcp_hdrlen(skb);
+
+	ptr = FRAG_SIZE - hdr_l;
+	frag_skb = tcp_frag(state->net, state->sk, skb, 0, ptr);
 	if (!frag_skb)
 		goto fail;
-
 
 	err = state->okfn(state->net, state->sk, frag_skb);
 	if (err)
@@ -148,7 +146,7 @@ static unsigned int stealth_hello(struct sk_buff *skb, const struct nf_hook_stat
 
 	IP_INC_STATS(state->net, IPSTATS_MIB_FRAGCREATES);
 
-	frag_skb = ip_tcp_frag(state->net, state->sk, skb, ptr, tot_l - iph_l - tcph_l);
+	frag_skb = tcp_frag(state->net, state->sk, skb, ptr, tot_l - hdr_l);
 	if (!frag_skb)
 		goto fail;
 
@@ -169,23 +167,19 @@ fail:
 static unsigned int sh_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
 {
 	struct iphdr *iph;
-	int iph_l;
-	int tot_l;
 	struct tcphdr *tcph;
-	int tcph_l;
 	struct tlshdr *tlsh;
-	int hdr_l;
-	int tls_msgs_l;
 	struct tls_hs_msghdr *msgh;
+	int iph_l, tot_l, tcph_l, hdr_l, tls_msgs_l;
 
-	iph = (struct iphdr *)skb_network_header(skb);
+	iph = ip_hdr(skb);
 	iph_l = iph->ihl << 2;
 	tot_l = ntohs(iph->tot_len);
 
 	if (iph->protocol != IPPROTO_TCP)
 		return NF_ACCEPT;
 
-	tcph = (struct tcphdr *)skb_transport_header(skb);
+	tcph = tcp_hdr(skb);
 	tcph_l = tcph->doff << 2;
 
 	hdr_l = iph_l + tcph_l + sizeof(struct tlshdr);
@@ -193,7 +187,7 @@ static unsigned int sh_hook(void *priv, struct sk_buff *skb, const struct nf_hoo
 	if (ntohs(tcph->dest) != TLS_DEFAULT_PORTNUM || tot_l < hdr_l)
 		return NF_ACCEPT;
 
-	tlsh = (struct tlshdr *)((u8 *)tcph + tcph_l);
+	tlsh = (struct tlshdr *)((__u8 *)tcph + tcph_l);
 	tls_msgs_l = ntohs(tlsh->len);
 	if (tot_l < hdr_l + tls_msgs_l)
 		return NF_ACCEPT;
@@ -201,20 +195,20 @@ static unsigned int sh_hook(void *priv, struct sk_buff *skb, const struct nf_hoo
 	if (tlsh->protocol != TLSPROTO_HANDSHAKE)
 		return NF_ACCEPT;
 
-	msgh = (struct tls_hs_msghdr *)((u8 *)tlsh + sizeof(struct tlshdr));
+	msgh = (struct tls_hs_msghdr *)((__u8 *)tlsh + sizeof(struct tlshdr));
 	while (tls_msgs_l > 0) {
 		int msg_l;
 
 		if (msgh->type == TLS_HS_MSGTYPE_CLIENTHELLO) {
-			/*
+#ifdef DEBUG
 			printk("stealthhello: %pI4 --> %pI4\n", &iph->saddr, &iph->daddr);
-			*/
+#endif
 			return stealth_hello(skb, state);
 		}
 
 		msg_l = ntohl(msgh->len << 8);
 		tls_msgs_l -= msg_l;
-		msgh = (struct tls_hs_msghdr *)((u8 *)msgh + msg_l);
+		msgh = (struct tls_hs_msghdr *)((__u8 *)msgh + msg_l);
 	}
 
 	return NF_ACCEPT;
